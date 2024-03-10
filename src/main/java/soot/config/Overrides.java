e429cef930e86b4f6c1e94c282dd309b80fe5005
MasterEnderman <xxmr.endermanxx@gmail.com>
2024-03-10 18:54:35 +0100

    overrides default to true

--- src/main/java/soot/config/Overrides.java
+++ src/main/java/soot/config/Overrides.java
@@ -6,50 +6,50 @@ public class Overrides {
     @Config.RequiresMcRestart
     @Config.Name("Ember Bore")
     @Config.Comment("Overrides the Ember Bore")
-    public boolean bore = false;
+    public boolean bore = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Dawnstone Anvil")
     @Config.Comment("Overrides the Dawnstone Anvil")
-    public boolean dawnstoneAnvil = false;
+    public boolean dawnstoneAnvil = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Hearth Coil")
     @Config.Comment("Overrides the Hearth Coil")
-    public boolean hearthCoil = false;
+    public boolean hearthCoil = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Mixer")
     @Config.Comment("Overrides the Mixer Centrifuge")
-    public boolean mixer = false;
+    public boolean mixer = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Stamper")
     @Config.Comment("Overrides the Stamper")
-    public boolean stamper = false;
+    public boolean stamper = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Beam Cannon")
     @Config.Comment("Overrides the Beam Cannon")
-    public boolean beamCannon = false;
+    public boolean beamCannon = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Alchemy Tablet")
     @Config.Comment("Overrides the Exchange Tablet")
-    public boolean alchemyTablet = false;
+    public boolean alchemyTablet = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Mech Accessor")
     @Config.Comment("Overrides the Mech Accessor")
-    public boolean mechAccessor = false;
+    public boolean mechAccessor = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Alchemy Pedestal")
     @Config.Comment("Overrides the Alchemy Pedestal")
-    public boolean alchemyPedestal = false;
+    public boolean alchemyPedestal = true;
 
     @Config.RequiresMcRestart
     @Config.Name("Crystal Cell")
     @Config.Comment("Overrides the Crystal Cell")
-    public boolean crystalCell = false;
+    public boolean crystalCell = true;
 }
